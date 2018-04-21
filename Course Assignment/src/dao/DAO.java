4896212940d5c7241260a666ec4f30524b40092f
ValentinValeanu <33149192+ValentinValeanu@users.noreply.github.com>
2018-04-21 21:35:23 +0200

    Database added
    Install postgresSQL
    https://www.enterprisedb.com/downloads/postgres-postgresql-downloads

--- Course Assignment/src/dao/DAO.java
+++ Course Assignment/src/dao/DAO.java
@@ -1,5 +1,37 @@
 package dao;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DAO 
+{
+	private Connection conn;
+	
+	public DAO()
+	{
+        try {
+        	
+			Class.forName("org.postgresql.Driver");
+	        
+	        DatabaseDetails d = DatabaseDetails.getInstance();
+	        
+	        conn = DriverManager.getConnection("jdbc:postgresql://" + d.getIPAddress() + ":5432/" + d.getDatabase(), d.getUserId(), d.getPassword());
+	        
+	        conn.setSchema(d.getSchema());
+	        
+        } catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		new DAO();
+	}
 
 }
