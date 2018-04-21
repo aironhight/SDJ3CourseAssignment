c03aca3ae050b34489cc873c68c36883416c2418
ValentinValeanu <33149192+ValentinValeanu@users.noreply.github.com>
2018-04-21 22:12:58 +0200

    Make the schema Database

--- Course Assignment/src/dao/DAO.java
+++ Course Assignment/src/dao/DAO.java
@@ -2,7 +2,9 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DAO 
 {
@@ -27,8 +29,36 @@ public class DAO
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+        
+        buildDDL();
 	}
 	
+	private void buildDDL() 
+	{
+		if (true) {
+			
+			
+			try {
+			
+				Statement stmt = conn.createStatement();
+				
+				stmt.executeUpdate("CREATE SCHEMA IF NOT EXISTS facility_schema");
+				
+				stmt.executeUpdate("CREATE TABLE cars (carID serial PRIMARY KEY, "
+						+ "make VARCHAR(10) not null, "
+						+ "model VARCHAR(10) not null, "
+						+ "year int not null,"
+						+ "VIN VARCHAR(10) not null,"
+						+ "weight read not null);");
+			
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
 	public static void main(String[] args)
 	{
 		new DAO();
