9a089d15447a537318cf9e5456ec8fa33f76245d
aironhight <aironhight@yahoo.com>
2018-04-21 14:12:20 +0200

    Fix

--- SDJ3Project/src/server/Server.java
+++ SDJ3Project/src/server/Server.java
@@ -1,11 +1,14 @@
 package server;
 
 import java.rmi.Remote;
+
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
 import java.util.List;
+import models.*;
 
 public interface Server extends Remote {
+	
 	String trackPart(String carVin) throws RemoteException;
 	void registerCar() throws RemoteException;
 }
